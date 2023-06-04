4ad091bfa65ca74693ec6d73712e1f6b6f4cf466
kasvrol <dovalekarolina@gmail.com>
2023-06-04 19:24:55 -0300

    file cleanup

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import "../../styles/FooterStyle.css";
+
+function App() {
+    return (
+        <div className="FooterContainer">
+            <p>Projeto criado com fins didáticos</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import "../../styles/HeaderStyle.css";
+
+function App() {
+    return (
+        <div className="HeaderContainer">
+            <p>Jogo de digitação</p>
+        </div>
+    );
+}
+
+export default App;
